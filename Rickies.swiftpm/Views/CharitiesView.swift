05f18838f8d87951aa217a73f5822c54b1b3c040
Jamie Blumberg <jamie_blumberg@icloud.com>
2022-12-15 09:30:24 -0500

    Add search capabilities to CharitiesView

--- Rickies.swiftpm/Views/CharitiesView.swift
+++ Rickies.swiftpm/Views/CharitiesView.swift
@@ -2,24 +2,23 @@ import SwiftUI
 
 struct CharitiesView: View {
     @ObservedObject var state: AppState
+    @State var searchText: String = ""
     var body: some View {
         VStack {
-            if let charities = state.charities {
-                List {
-                    ForEach(charities, id: \.charityName) { charity in
-                        Section {
-                            Text(charity.charityURL)
-                            Text("Donator: \(charity.donator)")
-                            Text("Donation Amount: $\(charity.donation)")
-                            Text(charity.game)
-                        } header: {
-                            Text(charity.charityName)
-                        }
+            List {
+                ForEach(searchResults, id: \.charityName) { charity in
+                    Section {
+                        Text(charity.charityURL)
+                        Text("Donator: \(charity.donator)")
+                        Text("Donation Amount: $\(charity.donation)")
+                        Text(charity.game)
+                    } header: {
+                        Text(charity.charityName)
                     }
                 }
             }
         }
-        .font(.title)
+        .searchable(text: $searchText, prompt: "Search by donator, charity or game")
         .task {
             do {
                 try await CharitiesFetcher.fetchCharities()
@@ -28,6 +27,20 @@ struct CharitiesView: View {
             }
         }
         .navigationTitle(Text("Charitable Donations"))
+    }
+    
+    var searchResults: [Charity] {
+        if searchText.isEmpty {
+            return state.charities ?? []
+        } else {
+            if let charities = state.charities {
+                return charities.filter { charity in
+                    charity.charityName.contains(searchText) ||
+                    charity.donator.contains(searchText) ||
+                    charity.game.contains(searchText)
+                }
+            } else  { return [] }
+        }
     }
 }
 
